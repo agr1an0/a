0e72e53cc20c2831fa12398b5a6c308db1b3e8a4
unknown <adrianofelixperera123@gmail.com>
2026-09-09 17:47:06 -0300

    first commit

---
+++ js/memoria.js
@@ -0,0 +1,55 @@
+const painel = document.querySelector('#painel')
+const x = {
+    image1: '',
+    image2: '',
+    image3: '',
+    image4: '',
+    image5: '',
+    image6: '',
+    image7: '',
+    image8: '',
+    image9: '',
+    image10: '',
+    image11: '',
+    image12: '',
+    image13: '',
+    image14: '',
+    image15: '',
+    image16: ''
+}
+const jogos = [x,y,z]
+
+{/*
+<h1 class="tituloJogo"></h1>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div>
+<div class="bloco" id=""></div> */}
