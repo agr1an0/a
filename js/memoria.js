9c40d15bf8220cf1b6a8c41428074f72953427ff
unknown <adrianofelixperera123@gmail.com>
2026-09-23 16:53:55 -0300

    segundo commit

--- js/memoria.js
+++ js/memoria.js
@@ -1,43 +1,100 @@
 const painel = document.querySelector('#painel')
-const x = {
-    image1: '',
-    image2: '',
-    image3: '',
-    image4: '',
-    image5: '',
-    image6: '',
-    image7: '',
-    image8: '',
-    image9: '',
-    image10: '',
-    image11: '',
-    image12: '',
-    image13: '',
-    image14: '',
-    image15: '',
-    image16: ''
+const fantasmas = {
+    "image1": '../',
+    "image2": '',
+    "image3": '',
+    "image4": '',
+    "image5": '',
+    "image6": '',
+    "image7": '',
+    "image8": '',
+    "image9": '',
+    "image10": '',
+    "image11": '',
+    "image12": '',
+    "image13": '',
+    "image14": '',
+    "image15": '',
+    "image16": ''
 }
-const jogos = [x,y,z]
+const magos = {
+    "image1": '',
+    "image2": '',
+    "image3": '',
+    "image4": '',
+    "image5": '',
+    "image6": '',
+    "image7": '',
+    "image8": '',
+    "image9": '',
+    "image10": '',
+    "image11": '',
+    "image12": '',
+    "image13": '',
+    "image14": '',
+    "image15": '',
+    "image16": ''
+}
+const cyberpunk = {
+    "image1": '',
+    "image2": '',
+    "image3": '',
+    "image4": '',
+    "image5": '',
+    "image6": '',
+    "image7": '',
+    "image8": '',
+    "image9": '',
+    "image10": '',
+    "image11": '',
+    "image12": '',
+    "image13": '',
+    "image14": '',
+    "image15": '',
+    "image16": ''
+}
+const tarot = {
+    "image1": '',
+    "image2": '',
+    "image3": '',
+    "image4": '',
+    "image5": '',
+    "image6": '',
+    "image7": '',
+    "image8": '',
+    "image9": '',
+    "image10": '',
+    "image11": '',
+    "image12": '',
+    "image13": '',
+    "image14": '',
+    "image15": '',
+    "image16": ''
+}
+const jogos = {fantasmas,magos,cyberpunk,tarot}
+for(let i = 0; i<=31;i++) {
+    painel.innerHTML += `<div class="bloco" id=${i}> <img src=${jogos.fantasmas[i]} alt=""></img> </div>`
+}
 
 {/*
-<h1 class="tituloJogo"></h1>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
-<div class="bloco" id=""></div>
+    <h1 class="tituloJogo"></h1>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
+    <div class="bloco" id=""></div>
 <div class="bloco" id=""></div>
 <div class="bloco" id=""></div>
 <div class="bloco" id=""></div>
